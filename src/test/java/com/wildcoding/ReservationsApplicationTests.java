b4350c3c7c2518ccc5933abdd6c6f89e501a5f3c
Krzysztof Surulo <krzysztof.surulo@gmail.com>
2019-11-21 04:20:55 +0000

    Add database files

---
+++ src/test/java/com/wildcoding/ReservationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
